618a7f1e91173a8da34ccc5ce379c5564cf85cf9
Mauroo Ciappina <ciappinamaurooj@gmail.com>
2023-06-29 15:25:49 -0300

    Html y rutas

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ const port = 3000;
 app.use(express.static('public'));
 
 
+app.get('/generic', (req, res) => {
+    res.sendFile(__dirname + '/public/generic.html');
+});
+app.get('/elements', (req, res) => {
+    res.sendFile(__dirname + '/public/elements.html');
+});
 
 app.get("*", (req, res) => {
     res.sendFile(__dirname + '/public/404.html');
