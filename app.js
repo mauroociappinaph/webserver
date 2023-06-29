10b6a545767852655c447c424834d9638650afe3
Mauroo Ciappina <ciappinamaurooj@gmail.com>
2023-06-29 16:26:36 -0300

    HTML

--- app.js
+++ app.js
@@ -2,20 +2,29 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.set('view engine', 'hbs');
+
 app.use(express.static('public'));
 
+app.get('/', (req, res) => {
+    res.render('./views/home.hbs', {
+        nombre: 'Mauroo Ciappina',
+        titulo: 'Viaje por Carretera',
+    });
+});
 
 app.get('/generic', (req, res) => {
-    res.sendFile(__dirname + '/public/generic.html');
-});
-app.get('/elements', (req, res) => {
-    res.sendFile(__dirname + '/public/elements.html');
+    res.sendFile(__dirname + '/public/back/Template/generic.html');
 });
 
-app.get("*", (req, res) => {
+app.get('/elements', (req, res) => {
+    res.sendFile(__dirname + '/public/back/Template/elements.html');
+});
+
+app.get('*', (req, res) => {
     res.sendFile(__dirname + '/public/404.html');
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log(`Escuchando en puerto ${port}!`);
 });
